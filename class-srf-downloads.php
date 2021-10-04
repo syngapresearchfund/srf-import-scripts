b7ca28f07e0559df60a42d9a034ada3babc8b6a9
Daniel W. Robert <danielwrobert@gmail.com>
2021-10-03 22:54:01 -0400

    Consolidate all download methods to one single method.

--- class-srf-downloads.php
+++ class-srf-downloads.php
@@ -2,10 +2,21 @@
 
 class SRF_Downloads {
 	private $data_set;
+	private $data_key;
+	private $output_path;
+	private $is_gallery;
 
-	public function __construct( $data_path ) {
+	public function __construct( $data_path, $data_key, $output_path, $is_gallery = false ) {
 		if ( ! is_string( $data_path ) ) {
 			echo 'Error: The data path must be passed in as a string!';
+			return; // exit early
+		}
+		if ( ! is_string( $data_key ) ) {
+			echo 'Error: The data key must be passed in as a string!';
+			return; // exit early
+		}
+		if ( ! is_string( $output_path ) ) {
+			echo 'Error: The output path must be passed in as a string!';
 			return; // exit early
 		}
 
@@ -14,149 +25,56 @@ class SRF_Downloads {
 			return; // exit early
 		}
 
-		$this->data_set = json_decode( file_get_contents( $data_path ), true );
+		$this->data_set    = json_decode( file_get_contents( $data_path ), true );
+		$this->data_key    = $data_key;
+		$this->output_path = $output_path;
+		$this->is_gallery  = $is_gallery;
 	}
 	
 	/**
-	 * Download warrior image galleries.
+	 * Download Webflow files from URL.
 	 *
 	 * @since 2021-10-03
 	 */
-	public function download_warrior_galleries() : void {
+	public function download_files() : void {
 		foreach ( $this->data_set as $key => $value ) {
-			$warrior_slug = $this->data_set[$key]['Slug'];
-			$image_array = explode( '; ', $this->data_set[$key]['Image Gallery'] );
-			$i = 0;
+			$item_slug = $this->data_set[$key]['Slug'];
+			$file_path = $this->is_gallery ? explode( '; ', $this->data_set[$key][$this->data_key] ) : $this->data_set[$key][$this->data_key];
 		
-			mkdir( "image-galleries/warriors/$warrior_slug", 0777, true );
+			mkdir( "$this->output_path/$item_slug", 0777, true );
 	
-			foreach ( $image_array as $image ) {
-				if ( empty( $image ) ) {
-					echo "The image gallery for $warrior_slug is empty.\n";
-					continue;
+			if ( empty( $file_path ) ) {
+				echo "The image path for $item_slug is empty.\n";
+				continue;
+			}
+
+			if ( $this->is_gallery ) {
+				$i = 0;
+
+				foreach ( $file_path as $file ) {
+					if ( empty( $file ) ) {
+						echo "The image gallery for $item_slug is empty.\n";
+						continue;
+					}
+		
+					$file_ext = substr( $file, -4 );
+		
+					file_put_contents( "$this->output_path/$item_slug/$item_slug-$i$file_ext", file_get_contents( $file ) );
+		
+					echo "Success! $item_slug-$i$file_ext was successfully downloaded.\n";
+		
+					$i++;
 				}
-	
-				$file_ext = substr( $image, -4 );
-	
-				file_put_contents( "image-galleries/warriors/$warrior_slug/$warrior_slug-$i$file_ext", file_get_contents( $image ) );
-	
-				echo "Success! $warrior_slug-$i$file_ext was successfully downloaded.\n";
-	
-				$i++;
+			} else {
+				$file_ext = substr( $file_path, -4 );
+
+				file_put_contents( "$this->output_path/$item_slug/$item_slug$file_ext", file_get_contents( $file_path ) );
+
+				echo "Success! The file for $item_slug has been downloaded.\n";
 			}
-	
-			echo "All images for $warrior_slug have been downloaded.\n";
 		}
 	
-		echo "Operation complete: All warrior image galleries have been downloaded to the proper directory. GREAT SUCCESS!";
-	}
-
-	/**
-	 * Download warrior featured images.
-	 *
-	 * @since 2021-10-03
-	 */
-	public function download_warrior_featured_images() : void {
-		foreach ( $this->data_set as $key => $value ) {
-			$warrior_slug = $this->data_set[$key]['Slug'];
-			$featured_image = $this->data_set[$key]['Primary Photo'];
-		
-			mkdir( "featured-images/warriors/$warrior_slug/featured-image", 0777, true );
-	
-			if ( empty( $featured_image ) ) {
-				echo "The image path for $warrior_slug is empty.\n";
-				continue;
-			}
-
-			$file_ext = substr( $featured_image, -4 );
-
-			file_put_contents( "featured-images/warriors/$warrior_slug/$warrior_slug-featured$file_ext", file_get_contents( $featured_image ) );
-
-			echo "Featured image for $warrior_slug has been downloaded.\n";
-		}
-	
-		echo "Operation complete: All warrior featured images have been downloaded to the proper directory. GREAT SUCCESS!";
-	}
-
-	/**
-	 * Download team member featured images.
-	 *
-	 * @since 2021-10-03
-	 */
-	public function download_team_featured_images() : void {
-		foreach ( $this->data_set as $key => $value ) {
-			$team_member_slug = $this->data_set[$key]['Slug'];
-			$featured_image = $this->data_set[$key]['Profile Picture'];
-		
-			mkdir( "featured-images/team-members/$team_member_slug", 0777, true );
-	
-			if ( empty( $featured_image ) ) {
-				echo "The image path for $team_member_slug is empty.\n";
-				continue;
-			}
-
-			$file_ext = substr( $featured_image, -4 );
-
-			file_put_contents( "featured-images/team-members/$team_member_slug/$team_member_slug-featured$file_ext", file_get_contents( $featured_image ) );
-
-			echo "Featured image for $team_member_slug has been downloaded.\n";
-		}
-	
-		echo "Operation complete: All team member featured images have been downloaded to the proper directory. GREAT SUCCESS!";
-	}
-
-	/**
-	 * Download researcher featured images.
-	 *
-	 * @since 2021-10-03
-	 */
-	public function download_researcher_featured_images() : void {
-		foreach ( $this->data_set as $key => $value ) {
-			$researcher_slug = $this->data_set[$key]['Slug'];
-			$featured_image = $this->data_set[$key]['Picture'];
-		
-			mkdir( "featured-images/researchers/$researcher_slug", 0777, true );
-	
-			if ( empty( $featured_image ) ) {
-				echo "The image path for $researcher_slug is empty.\n";
-				continue;
-			}
-
-			$file_ext = substr( $featured_image, -4 );
-
-			file_put_contents( "featured-images/researchers/$researcher_slug/$researcher_slug-featured$file_ext", file_get_contents( $featured_image ) );
-
-			echo "Featured image for $researcher_slug has been downloaded.\n";
-		}
-	
-		echo "Operation complete: All researcher featured images have been downloaded to the proper directory. GREAT SUCCESS!";
-	}
-
-	/**
-	 * Download blog featured images.
-	 *
-	 * @since 2021-10-03
-	 */
-	public function download_blog_featured_images() : void {
-		foreach ( $this->data_set as $key => $value ) {
-			$post_slug = $this->data_set[$key]['Slug'];
-			$featured_image = $this->data_set[$key]['Main Image'];
-		
-			mkdir( "featured-images/blog/$post_slug", 0777, true );
-	
-			if ( empty( $featured_image ) ) {
-				echo "The image path for $post_slug is empty.\n";
-				continue;
-			}
-
-			$file_ext = substr( $featured_image, -4 );
-
-			file_put_contents( "featured-images/blog/$post_slug/$post_slug-featured$file_ext", file_get_contents( $featured_image ) );
-
-			echo "Featured image for $post_slug has been downloaded.\n";
-		}
-	
-		echo "Operation complete: All blog post featured images have been downloaded to the proper directory. GREAT SUCCESS!";
+		echo "Operation complete: All files have been downloaded to the proper directory. GREAT SUCCESS!";
 	}
 }
 
@@ -172,5 +90,11 @@ class SRF_Downloads {
 // $researcher_featured_images = new SRF_Downloads( 'data/webflow-json/SRF-Researchers.json' );
 // $researcher_featured_images->download_researcher_featured_images();
 
-$blog_featured_images = new SRF_Downloads( 'data/webflow-json/SRF-Blog-Posts.json' );
-$blog_featured_images->download_blog_featured_images();
+// $blog_featured_images = new SRF_Downloads( 'data/webflow-json/SRF-Blog-Posts.json' );
+// $blog_featured_images->download_blog_featured_images();
+
+// $event_featured_images = new SRF_Downloads( 'data/webflow-json/SRF-Events.json', 'Image', 'images/events' );
+// $event_featured_images->download_files();
+
+$warrior_galleries = new SRF_Downloads( 'data/webflow-json/SRF-Warriors.json', 'Image Gallery', 'images/warriors/galleries', true );
+$warrior_galleries->download_files();
