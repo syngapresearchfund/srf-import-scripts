fd379777529bed66b9fef56a5e05400a27940a2b
Daniel W. Robert <danielwrobert@gmail.com>
2021-10-03 20:55:26 -0400

    Refactor to DRY duplicate code.

--- class-srf-content-imports.php
+++ class-srf-content-imports.php
@@ -3,29 +3,27 @@
 require_once '../wp-load.php';
 
 class SRF_Content_Imports {
-	private $data_path;
+	private $data_set;
 
 	public function __construct( $data_path ) {
-		$this->data_path = $data_path;
-	}
-
-	// Post Categories:
-	public function import_post_categories() : void {
-		if ( ! is_string( $this->data_path ) ) {
+		if ( ! is_string( $data_path ) ) {
 			echo 'Error: The data path must be passed in as a string!';
 			return; // exit early
 		}
 
-		$post_categories = json_decode( file_get_contents( $this->data_path ), true );
+		$this->data_set = json_decode( file_get_contents( $data_path ), true );
+	}
 
-		foreach ( $post_categories as $key => $value ) {
-			// echo $post_categories[$key]['Name'] . "\n";
+	// Post Categories:
+	public function import_post_categories() : void {
+		foreach ( $data_set as $key => $value ) {
+			// echo $data_set[$key]['Name'] . "\n";
 			wp_insert_term(
-				$post_categories[$key]['Name'],
+				$data_set[$key]['Name'],
 				'category',
 				array(
-					'description' => $post_categories[$key]['Description'],
-					'slug' => $post_categories[$key]['Slug'],
+					'description' => $data_set[$key]['Description'],
+					'slug' => $data_set[$key]['Slug'],
 				)
 			);
 		}
@@ -33,23 +31,16 @@ class SRF_Content_Imports {
 
 	// Posts:
 	public function import_posts() : void {
-		if ( ! is_string( $this->data_path ) ) {
-			echo 'Error: The data path must be passed in as a string!';
-			return; // exit early
-		}
-
-		$posts = json_decode( file_get_contents( $this->data_path ), true );
-
-		foreach ( $posts as $key => $value ) {
-			// echo $posts[$key]['Name'] . "\n";
-			$formatted_date = strtotime( substr( $posts[$key]['Published On'], 0, -29 ) );
+		foreach ( $data_set as $key => $value ) {
+			// echo $data_set[$key]['Name'] . "\n";
+			$formatted_date = strtotime( substr( $data_set[$key]['Published On'], 0, -29 ) );
 
 			$args = array(
 				'post_author'   => 1,
 				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $posts[$key]['Name'],
-				'post_name'    => $posts[$key]['Slug'],
-				'post_content'  => $posts[$key]['Post Body'],
+				'post_title'    => $data_set[$key]['Name'],
+				'post_name'    => $data_set[$key]['Slug'],
+				'post_content'  => $data_set[$key]['Post Body'],
 				// 'post_category' => array( 5 ),
 				'comment_status' => 'closed',
 				'ping_status' => 'closed',
@@ -62,22 +53,15 @@ class SRF_Content_Imports {
 
 	// Warriors:
 	public function import_warriors() : void {
-		if ( ! is_string( $this->data_path ) ) {
-			echo 'Error: The data path must be passed in as a string!';
-			return; // exit early
-		}
-
-		$warriors = json_decode( file_get_contents( $this->data_path ), true );
-
-		foreach ( $warriors as $key => $value ) {
-			$formatted_date = strtotime( substr( $warriors[$key]['Published On'], 0, -29 ) );
+		foreach ( $data_set as $key => $value ) {
+			$formatted_date = strtotime( substr( $data_set[$key]['Published On'], 0, -29 ) );
 
 			$args = array(
 				'post_author'   => 1,
 				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $warriors[$key]['Title'],
-				'post_name'    => $warriors[$key]['Slug'],
-				'post_content'  => $warriors[$key]['Full Story'],
+				'post_title'    => $data_set[$key]['Title'],
+				'post_name'    => $data_set[$key]['Slug'],
+				'post_content'  => $data_set[$key]['Full Story'],
 				// TODO: Set post category to an argument
 				// 'post_category' => array( 8 ),
 				'comment_status' => 'closed',
@@ -92,30 +76,23 @@ class SRF_Content_Imports {
 
 	// Team:
 	public function import_team() : void {
-		if ( ! is_string( $this->data_path ) ) {
-			echo 'Error: The data path must be passed in as a string!';
-			return; // exit early
-		}
-
-		$team = json_decode( file_get_contents( $this->data_path ), true );
-
-		foreach ( $team as $key => $value ) {
-			$formatted_date = strtotime( substr( $team[$key]['Published On'], 0, -29 ) );
-			$post_content = $team[$key]['Full Bio'] . "\n";
-			$post_content .= 'Job Title: ' . $team[$key]['Job Title'] . "\n";
-			$post_content .= 'Team: ' . $team[$key]['Team'] . "\n";
-			$post_content .= 'Email: ' . $team[$key]['Email'] . "\n";
-			$post_content .= 'Phone Number: ' . $team[$key]['Phone Number'] . "\n";
-			$post_content .= 'Twitter: ' . $team[$key]['Twitter Link'] . "\n";
-			$post_content .= 'Facebook: ' . $team[$key]['Facebook Link'] . "\n";
-			$post_content .= 'LinkedIn: ' . $team[$key]['LinkedIn'];
+		foreach ( $data_set as $key => $value ) {
+			$formatted_date = strtotime( substr( $data_set[$key]['Published On'], 0, -29 ) );
+			$post_content = $data_set[$key]['Full Bio'] . "\n";
+			$post_content .= 'Job Title: ' . $data_set[$key]['Job Title'] . "\n";
+			$post_content .= 'Team: ' . $data_set[$key]['Team'] . "\n";
+			$post_content .= 'Email: ' . $data_set[$key]['Email'] . "\n";
+			$post_content .= 'Phone Number: ' . $data_set[$key]['Phone Number'] . "\n";
+			$post_content .= 'Twitter: ' . $data_set[$key]['Twitter Link'] . "\n";
+			$post_content .= 'Facebook: ' . $data_set[$key]['Facebook Link'] . "\n";
+			$post_content .= 'LinkedIn: ' . $data_set[$key]['LinkedIn'];
 
 			$args = array(
 				'post_author'   => 1,
 				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $team[$key]['Name'],
-				'post_name'    => $team[$key]['Slug'],
-				'post_excerpt'  => $team[$key]['Bio Summary'],
+				'post_title'    => $data_set[$key]['Name'],
+				'post_name'    => $data_set[$key]['Slug'],
+				'post_excerpt'  => $data_set[$key]['Bio Summary'],
 				'post_content'  => $post_content,
 				// TODO: Set post category to an argument
 				// 'post_category' => array( 9 ),
@@ -131,26 +108,19 @@ class SRF_Content_Imports {
 
 	// Researchers:
 	public function import_researchers() : void {
-		if ( ! is_string( $this->data_path ) ) {
-			echo 'Error: The data path must be passed in as a string!';
-			return; // exit early
-		}
-
-		$researchers = json_decode( file_get_contents( $this->data_path ), true );
-
-		foreach ( $researchers as $key => $value ) {
-			$formatted_date = strtotime( substr( $researchers[$key]['Published On'], 0, -29 ) );
-			$post_content = $researchers[$key]['Bio Summary'] . "\n";
-			$post_content .= 'Researcher URI: ' . $researchers[$key]['External Link'] . "\n";
-			$post_content .= 'Institution: ' . $researchers[$key]['Institution'] . "\n";
-			$post_content .= 'Institution URI: ' . $researchers[$key]['Institution Link'] . "\n";
-			$post_content .= 'SAB Member: ' . ( $researchers[$key]['SAB Member?'] ? 'Yes' : "No" );
+		foreach ( $data_set as $key => $value ) {
+			$formatted_date = strtotime( substr( $data_set[$key]['Published On'], 0, -29 ) );
+			$post_content = $data_set[$key]['Bio Summary'] . "\n";
+			$post_content .= 'Researcher URI: ' . $data_set[$key]['External Link'] . "\n";
+			$post_content .= 'Institution: ' . $data_set[$key]['Institution'] . "\n";
+			$post_content .= 'Institution URI: ' . $data_set[$key]['Institution Link'] . "\n";
+			$post_content .= 'SAB Member: ' . ( $data_set[$key]['SAB Member?'] ? 'Yes' : "No" );
 
 			$args = array(
 				'post_author'   => 1,
 				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $researchers[$key]['Name'],
-				'post_name'    => $researchers[$key]['Slug'],
+				'post_title'    => $data_set[$key]['Name'],
+				'post_name'    => $data_set[$key]['Slug'],
 				'post_content'  => $post_content,
 				// TODO: Set post category to an argument
 				// 'post_category' => array( 9 ),
--- class-srf-downloads.php
+++ class-srf-downloads.php
@@ -1,14 +1,10 @@
 <?php
 
 class SRF_Downloads {
-	private $data_path;
+	private $data_set;
 
 	public function __construct( $data_path ) {
-		$this->data_path = $data_path;
-	}
-	
-	public function download_warrior_gallery() : void {
-		if ( ! is_string( $this->data_path || empty( $data_path ) ) ) {
+		if ( ! is_string( $data_path ) ) {
 			echo 'Error: The data path must be passed in as a string!';
 			return; // exit early
 		}
@@ -17,12 +13,21 @@ class SRF_Downloads {
 			echo 'Error: allow_url_fopen is not enabled in your environment!';
 			return; // exit early
 		}
+
+		$this->data_set = json_decode( file_get_contents( $data_path ), true );
+	}
 	
-		$warriors = json_decode( file_get_contents( $this->data_path ), true );
+	/**
+	 * Download warrior image gallery.
+	 *
+	 * @since 2021-10-03
+	 */
+	public function download_warrior_gallery() : void {
+		$data_set = json_decode( file_get_contents( $this->data_path ), true );
 	
-		foreach ( $warriors as $key => $value ) {
-			$warrior_slug = $warriors[$key]['Slug'];
-			$image_array = explode( '; ', $warriors[$key]['Image Gallery'] );
+		foreach ( $data_set as $key => $value ) {
+			$warrior_slug = $data_set[$key]['Slug'];
+			$image_array = explode( '; ', $data_set[$key]['Image Gallery'] );
 			$i = 0;
 		
 			mkdir( "images/warriors/$warrior_slug", 0777, true );
@@ -47,7 +52,39 @@ class SRF_Downloads {
 	
 		echo "Operation complete: All Warrior images have been downloaded to the proper directory. GREAT SUCCESS!";
 	}
+
+	/**
+	 * Download warrior featured image.
+	 *
+	 * @since 2021-10-03
+	 */
+	public function download_warrior_featured_image() : void {
+		$data_set = json_decode( file_get_contents( $this->data_path ), true );
+	
+		foreach ( $data_set as $key => $value ) {
+			$warrior_slug = $data_set[$key]['Slug'];
+			$featured_image = $data_set[$key]['Image Gallery'];
+		
+			mkdir( "images/warriors/$warrior_slug/featured-image", 0777, true );
+	
+			if ( empty( $featured_image ) ) {
+				echo "The image gallery for $warrior_slug is empty.\n";
+				continue;
+			}
+
+			$file_ext = substr( $image, -4 );
+
+			file_put_contents( "images/warriors/$warrior_slug/$warrior_slug-featured$file_ext", file_get_contents( $image ) );
+
+			echo "Featured image for $warrior_slug has been downloaded.\n";
+		}
+	
+		echo "Operation complete: All Warrior images have been downloaded to the proper directory. GREAT SUCCESS!";
+	}
 }
 
 // $warrior_gallery = new SRF_Downloads( 'data/webflow-json/SRF-Warriors.json' );
 // $warrior_gallery->download_warrior_gallery();
+
+$warrior_featured_image = new SRF_Downloads( 'data/webflow-json/SRF-Warriors.json' );
+$warrior_featured_image->download_warrior_featured_image();
