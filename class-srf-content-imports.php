010d1c054b4ced719594785d065fff5cef9f495c
Daniel W. Robert <danielwrobert@gmail.com>
2021-10-09 22:31:41 -0400

    Update content imports for API data format.

--- class-srf-content-imports.php
+++ class-srf-content-imports.php
@@ -21,11 +21,11 @@ class SRF_Content_Imports {
 		foreach ( $this->data_set as $key => $value ) {
 			// echo $this->data_set[$key]['Name'] . "\n";
 			wp_insert_term(
-				$this->data_set[$key]['Name'],
+				$this->data_set[$key]['name'],
 				'category',
 				array(
-					'description' => $this->data_set[$key]['Description'],
-					'slug' => $this->data_set[$key]['Slug'],
+					'description' => $this->data_set[$key]['description'],
+					'slug' => $this->data_set[$key]['slug'],
 				)
 			);
 		}
@@ -35,14 +35,16 @@ class SRF_Content_Imports {
 	public function import_posts() : void {
 		foreach ( $this->data_set as $key => $value ) {
 			// echo $this->data_set[$key]['Name'] . "\n";
-			$formatted_date = strtotime( substr( $this->data_set[$key]['Published On'], 0, -29 ) );
+			$data           = $this->data_set[$key];
+			// $formatted_date = strtotime( substr( $data['published-on'], 0, -29 ) );
+			$formatted_date = strtotime( $data['published-on'] );
 
 			$args = array(
 				'post_author'   => 1,
 				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $this->data_set[$key]['Name'],
-				'post_name'    => $this->data_set[$key]['Slug'],
-				'post_content'  => $this->data_set[$key]['Post Body'],
+				'post_title'    => $data['name'],
+				'post_name'    => $data['slug'],
+				'post_content'  => $data['post-body'],
 				// 'post_category' => array( 5 ),
 				'comment_status' => 'closed',
 				'ping_status' => 'closed',
@@ -56,20 +58,24 @@ class SRF_Content_Imports {
 	// Warriors:
 	public function import_warriors() : void {
 		foreach ( $this->data_set as $key => $value ) {
-			$formatted_date = strtotime( substr( $this->data_set[$key]['Published On'], 0, -29 ) );
+			$data           = $this->data_set[$key];
+			// $formatted_date = strtotime( substr( $data['publication-date'], 0, -29 ) );
+			$formatted_date = strtotime( $data['publication-date'] );
+
+			$post_content  = '<strong>' . $data['age'] . "</strong>\n";
+			$post_content .= '<strong>' . $data['location'] . "</strong>\n";
+			$post_content .= isset( $data['full-story'] ) ? $data['full-story'] . "\n" : '';
 
 			$args = array(
-				'post_author'   => 1,
-				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $this->data_set[$key]['Title'],
-				'post_name'    => $this->data_set[$key]['Slug'],
-				'post_content'  => $this->data_set[$key]['Full Story'],
-				// TODO: Set post category to an argument
-				// 'post_category' => array( 8 ),
+				'post_author'    => 1,
+				'post_date'      => date( 'Y-m-d H:i:s', $formatted_date ),
+				'post_title'     => $data['name'],
+				'post_name'      => $data['slug'],
+				'post_content'   => $post_content,
 				'comment_status' => 'closed',
-				'ping_status' => 'closed',
-				'post_status' => 'publish',
-				'post_type' => 'srf-people',
+				'ping_status'    => 'closed',
+				'post_status'    => 'publish',
+				'post_type'      => 'srf-people',
 			);
 
 			wp_insert_post( $args );
@@ -79,29 +85,28 @@ class SRF_Content_Imports {
 	// Team:
 	public function import_team() : void {
 		foreach ( $this->data_set as $key => $value ) {
-			$formatted_date = strtotime( substr( $this->data_set[$key]['Published On'], 0, -29 ) );
-			$post_content = $this->data_set[$key]['Full Bio'] . "\n";
-			$post_content .= 'Job Title: ' . $this->data_set[$key]['Job Title'] . "\n";
-			$post_content .= 'Team: ' . $this->data_set[$key]['Team'] . "\n";
-			$post_content .= 'Email: ' . $this->data_set[$key]['Email'] . "\n";
-			$post_content .= 'Phone Number: ' . $this->data_set[$key]['Phone Number'] . "\n";
-			$post_content .= 'Twitter: ' . $this->data_set[$key]['Twitter Link'] . "\n";
-			$post_content .= 'Facebook: ' . $this->data_set[$key]['Facebook Link'] . "\n";
-			$post_content .= 'LinkedIn: ' . $this->data_set[$key]['LinkedIn'];
+			$data           = $this->data_set[$key];
+			// $formatted_date = strtotime( substr( $data['published-on'], 0, -29 ) );
+			$formatted_date = strtotime( $data['published-on'] );
+
+			$post_content  = isset( $data['job-title'] ) ? '<h2>' . $data['job-title'] . "</h2>\n" : '';
+			$post_content .= $data['bio'] . "\n";
+			$post_content .= isset( $data['email'] ) ? 'Email:' . $data['email'] . "\n" : '';
+			$post_content .= isset( $data['twitter-link'] ) ? 'Twitter:' . $data['twitter-link'] . "\n" : '';
+			$post_content .= isset( $data['facebook-link'] ) ? 'Facebook:' . $data['facebook-link'] . "\n" : '';
+			$post_content .= isset( $data['linkedin'] ) ? 'LinkedIn:' . $data['linkedin'] . "\n" : '';
 
 			$args = array(
-				'post_author'   => 1,
-				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $this->data_set[$key]['Name'],
-				'post_name'    => $this->data_set[$key]['Slug'],
-				'post_excerpt'  => $this->data_set[$key]['Bio Summary'],
-				'post_content'  => $post_content,
-				// TODO: Set post category to an argument
-				// 'post_category' => array( 9 ),
+				'post_author'    => 1,
+				'post_date'      => date( 'Y-m-d H:i:s', $formatted_date ),
+				'post_title'     => $data['name'],
+				'post_name'      => $data['slug'],
+				'post_excerpt'   => isset( $data['bio-summary'] ) ?: '',
+				'post_content'   => $post_content,
 				'comment_status' => 'closed',
-				'ping_status' => 'closed',
-				'post_status' => 'publish',
-				'post_type' => 'srf-people',
+				'ping_status'    => 'closed',
+				'post_status'    => 'publish',
+				'post_type'      => 'srf-people',
 			);
 
 			wp_insert_post( $args );
@@ -111,25 +116,26 @@ class SRF_Content_Imports {
 	// Researchers:
 	public function import_researchers() : void {
 		foreach ( $this->data_set as $key => $value ) {
-			$formatted_date = strtotime( substr( $this->data_set[$key]['Published On'], 0, -29 ) );
-			$post_content = $this->data_set[$key]['Bio Summary'] . "\n";
-			$post_content .= 'Researcher URI: ' . $this->data_set[$key]['External Link'] . "\n";
-			$post_content .= 'Institution: ' . $this->data_set[$key]['Institution'] . "\n";
-			$post_content .= 'Institution URI: ' . $this->data_set[$key]['Institution Link'] . "\n";
-			$post_content .= 'SAB Member: ' . ( $this->data_set[$key]['SAB Member?'] ? 'Yes' : "No" );
+			$data           = $this->data_set[$key];
+			// $formatted_date = strtotime( substr( $data['published-on'], 0, -29 ) );
+			$formatted_date = strtotime( $data['published-on'] );
+
+			$post_content  = isset( $data['bio-summary'] ) ? $data['bio-summary'] . "\n" : '';
+			$post_content .= isset( $data['external-link'] ) ? 'Website: ' . $data['external-link'] . "\n" : '';
+			$post_content .= isset( $data['institution'] ) ? 'Institution: ' . $data['institution'] . "\n" : '';
+			$post_content .= isset( $data['institution-link'] ) ? 'Institution Website: ' . $data['institution-link'] . "\n" : '';
+			$post_content .= 'SAB Member: ' . ( $data['sab-member'] ? 'Yes' : "No" );
 
 			$args = array(
-				'post_author'   => 1,
-				'post_date' => date( 'Y-m-d H:i:s', $formatted_date ),
-				'post_title'    => $this->data_set[$key]['Name'],
-				'post_name'    => $this->data_set[$key]['Slug'],
-				'post_content'  => $post_content,
-				// TODO: Set post category to an argument
-				// 'post_category' => array( 9 ),
+				'post_author'    => 1,
+				'post_date'      => date( 'Y-m-d H:i:s', $formatted_date ),
+				'post_title'     => $data['name'],
+				'post_name'      => $data['slug'],
+				'post_content'   => $post_content,
 				'comment_status' => 'closed',
-				'ping_status' => 'closed',
-				'post_status' => 'publish',
-				'post_type' => 'srf-people',
+				'ping_status'    => 'closed',
+				'post_status'    => 'publish',
+				'post_type'      => 'srf-people',
 			);
 
 			wp_insert_post( $args );
@@ -138,81 +144,50 @@ class SRF_Content_Imports {
 
 	// Events:
 	public function import_events(): void {
-	// 	foreach ( $this->data_set as $key => $value ) {
-	// 		// echo $this->data_set[$key]['name'] . "\n";
-	// 		$formatted_date        = strtotime( substr($this->data_set[$key]['Created On'], 0, -29 ) );
-	// 		// $formatted_event_date  = strtotime( substr($this->data_set[$key]['Start Date/Time'], 0, -29 ) );
-	// 		$event_description     = $this->data_set[$key]['Short Description'];
-	// 		$is_published          = $this->data_set[$key]['Published On'];
-
-	// 		$post_content = "<h3>Event Time</h3>\n";
-	// 		$post_content .= $this->data_set[$key]['Start Date/Time Display'] . "\n";
-	// 		$post_content .= !empty( $event_description ) ? $event_description . "\n" : '';
-	// 		$post_content .= "<h3>RSVP</h3>\n";
-	// 		$post_content .= $this->data_set[$key]['RSVP Link'] . "\n";
-
-	// 		$args = array(
-	// 			'post_author'   => 1,
-	// 			'post_date' => date('Y-m-d H:i:s', $formatted_date),
-	// 			'post_title'    => $this->data_set[$key]['Name'],
-	// 			'post_name'    => $this->data_set[$key]['Slug'],
-	// 			'post_content'  => $post_content,
-	// 			// TODO: Set post category to an argument
-	// 			// 'post_category' => array( 9 ),
-	// 			'comment_status' => 'closed',
-	// 			'ping_status' => 'closed',
-	// 			'post_status' => ! empty( $is_published ) ? 'publish' : 'draft',
-	// 			'post_type' => 'srf-events',
-	// 		);
-
-	// 		wp_insert_post( $args );
-	// 	}
-
 		foreach ( $this->data_set as $key => $value ) {
 			// echo $this->data_set[$key]['name'] . "\n";
-			$data_obj = $this->data_set[$key];
-			$formatted_date        = strtotime( substr($data_obj['created-on'], 0, -29 ) );
-			// $formatted_event_date  = strtotime( substr($data_obj['Start Date/Time'], 0, -29 ) );
-			$event_description     = isset( $data_obj['short-description'] ) ? $data_obj['short-description'] : '';
-			$is_published          = $data_obj['published-on'];
+			$data              = $this->data_set[$key];
+			// $formatted_date    = strtotime( substr($data['created-on'], 0, -29 ) );
+			$formatted_date    = strtotime( $data['created-on'] );
+			$event_description = isset( $data['short-description'] ) ? $data['short-description'] : '';
+			$is_published      = $data['published-on'];
 
-			$post_content = "<h3>Event Time</h3>\n";
-			$post_content .= $data_obj['start-date-time-display'] . "\n";
+			$post_content  = "<h3>Event Time</h3>\n";
+			$post_content .= $data['start-date-time-display'] . "\n";
 			$post_content .= ! empty( $event_description ) ? "<h3>Description</h3>\n" . $event_description . "\n" : '';
-			$post_content .= isset( $data_obj['rsvp-link'] ) ? '<h3>RSVP</h3><a href="' . $data_obj['rsvp-link'] . '">' . $data_obj['rsvp-link'] . '</a>' : '';
+			$post_content .= isset( $data['rsvp-link'] ) ? '<h3>RSVP</h3><a href="' . $data['rsvp-link'] . '">' . $data['rsvp-link'] . '</a>' : '';
 
 			$args = array(
-				'post_author'   => 1,
-				'post_date' => date('Y-m-d H:i:s', $formatted_date),
-				'post_title'    => $data_obj['name'],
-				'post_name'    => $data_obj['slug'],
-				'post_content'  => $post_content,
-				// TODO: Set post category to an argument
-				// 'post_category' => array( 9 ),
+				'post_author'    => 1,
+				'post_date'      => date('Y-m-d H:i:s', $formatted_date),
+				'post_title'     => $data['name'],
+				'post_name'      => $data['slug'],
+				'post_content'   => $post_content,
 				'comment_status' => 'closed',
-				'ping_status' => 'closed',
-				'post_status' => ! empty( $is_published ) ? 'publish' : 'draft',
-				'post_type' => 'srf-events',
+				'ping_status'    => 'closed',
+				'post_status'    => ! empty( $is_published ) ? 'publish' : 'draft',
+				'post_type'      => 'srf-events',
 			);
 
 			wp_insert_post( $args );
 		}
 	}
 }
-// $post_categories = new SRF_Content_Imports( './data/webflow-json/SRF-Blog-Categories.json' );
-// $post_categories->import_post_categories();
+// $post_categories = new SRF_Content_Imports( './data/webflow-api-data/api-srf-blog-categories.json' );
+// $post_categories->import_posts();
 
-// $posts = new SRF_Content_Imports( './data/webflow-json/SRF-Blog-Posts.json' );
+// $posts = new SRF_Content_Imports( './data/webflow-api-data/api-srf-posts.json' );
 // $posts->import_posts();
 
-// $warriors = new SRF_Content_Imports( './data/webflow-json/SRF-Warriors.json' );
+// $warriors = new SRF_Content_Imports( './data/webflow-api-data/api-srf-warriors.json' );
+// $warriors = new SRF_Content_Imports( './data/webflow-api-data/api-srf-warriors-2.json' );
 // $warriors->import_warriors();
 
-// $team = new SRF_Content_Imports( './data/webflow-json/SRF-Team-Members.json' );
+// $team = new SRF_Content_Imports( './data/webflow-api-data/api-srf-team.json' );
 // $team->import_team();
 
-// $researchers = new SRF_Content_Imports( './data/webflow-json/SRF-Researchers.json' );
+// $researchers = new SRF_Content_Imports( './data/webflow-api-data/api-srf-researchers.json' );
 // $researchers->import_researchers();
 
-$events = new SRF_Content_Imports( './data/webflow-api-data/api-srf-events.json' );
-$events->import_events();
+// $events = new SRF_Content_Imports( './data/webflow-api-data/api-srf-events.json' );
+// $events->import_events();
